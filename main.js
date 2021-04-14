02ba956aa7bbc79e9e0a4bb425531663e418497b
Vadim Kosenkov <vadimkosenkov.work@gmail.com>
2021-04-14 16:50:52 +0300

    feat (main.js) Add links color selected

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-setInterval(onIntervalNextTick, 1000);
-
 const htmlElements = {};
 htmlElements.startBtn = document.querySelector('.container .buttons button.start');
 htmlElements.stopBtn = document.querySelector('.container .buttons button.stop');
@@ -8,9 +6,27 @@ htmlElements.clock = document.querySelector('.container .links .clock');
 htmlElements.stopwatch = document.querySelector('.container .links .stopwatch');
 htmlElements.timer = document.querySelector('.container .links .timer');
 htmlElements.output = document.querySelector('.container .output');
+htmlElements.links = document.querySelectorAll('.container .links a');
+const links = Array.from(htmlElements.links)
 
+function init() {
+    onIntervalNextTick()
+    linksEvent()
+    setInterval(onIntervalNextTick, 1000);
+}
+
+init()
 
 function onIntervalNextTick() {
     const date = new Date().toLocaleTimeString()
     htmlElements.output.innerText = date
+}
+
+function linksEvent() {
+    links.forEach((e) => e.addEventListener('click', linksFunc))
+}
+
+function linksFunc(event) {
+    links.forEach((e) => e.classList.remove('selected'))
+    event.target.classList.add('selected')
 }
